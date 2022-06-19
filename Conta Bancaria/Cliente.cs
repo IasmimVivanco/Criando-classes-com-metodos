0df858959b2121dec3e021cc05859f3947dba142
IasmimVivanco <iasmim.vivanco@hotmail.com>
2022-06-19 12:24:56 -0300

    Subindo novo exercício

---
+++ Conta Bancaria/Cliente.cs
@@ -0,0 +1,54 @@
+﻿using System.Globalization;
+namespace Conta_Bancaria
+{
+    internal class Cliente
+    {
+        public string Titular { get; set; }
+        public int NumeroDaConta { get; private set; }
+        public double Saldo { get; private set; }
+
+        public Cliente(string titular, int numeroDaConta)
+        {
+            Titular = titular;
+            NumeroDaConta = numeroDaConta;
+        }
+
+        public Cliente(string titular, int numeroDaConta, double saldo)
+        {
+            Titular = titular;
+            NumeroDaConta = numeroDaConta;
+            Saldo = saldo;
+        }
+
+        public double DepositoInicial() {
+            char depositoInicial = char.Parse(Console.ReadLine());
+            if (depositoInicial == 's')
+            {
+                Console.Write("Digite o valor do depósito inicial: ");
+                double valor = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                return Saldo = valor;
+            }
+            else
+            {
+                return Saldo = 0.0;
+            }
+}
+        public double Deposito()
+        {
+            double deposito = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            return Saldo += deposito;
+        }
+        public double saque()
+        {
+            double Saque = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Saldo = Saldo - 5.00;
+            return Saldo -= Saque;
+        }
+
+        public override string ToString()
+        {
+            return ($"Conta: {NumeroDaConta}, Titular: {Titular}, Saldo: ${Saldo.ToString("F2",CultureInfo.InvariantCulture)}");
+        }
+        
+    }
+}
---
+++ Conta Bancaria/Program.cs
@@ -0,0 +1,43 @@
+﻿using System.Globalization;
+namespace Conta_Bancaria
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Cliente cliente;
+
+            Console.Write("Digite o numero da conta: ");
+            int numeroDaConta = int.Parse(Console.ReadLine());
+           
+            Console.Write("Digite o nome do titular da conta: ");
+            string titular = Console.ReadLine();
+
+            cliente = new Cliente(titular, numeroDaConta);
+
+            Console.Write("Haverá depósito inicial (s/n) ? ");
+            cliente.DepositoInicial();
+            Console.WriteLine();
+
+            Console.WriteLine("Dados da conta: ");
+            Console.WriteLine(cliente);
+            Console.WriteLine();
+           
+            Console.Write("Digite o valor de depósito: ");
+            double saldo = cliente.Deposito();
+
+            cliente = new Cliente(titular, numeroDaConta, saldo);
+
+            Console.WriteLine("Dados da conta atualizados: ");
+            Console.WriteLine(cliente);
+            Console.WriteLine();
+            
+            Console.Write("Digite o valor para saque: ");
+   
+            cliente.saque();
+            Console.WriteLine("Dados da conta atualizados: ");
+            Console.WriteLine(cliente);
+
+        }
+    }
+}
